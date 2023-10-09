c703e2f7125807678c06838279e9a2fe81349e21
Allan-Wachira <aldocky@gmail.com>
2023-10-09 00:28:38 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < 16; i++)
-	{
-		char hex_char = (i < 10) ? ('0' + i) : ('a' + i - 10);
-		putchar(hex_char);
-	}
+    for (i = 0; i < 16; i++)
+    {
+        char hex_char = (i < 10) ? ('0' + i) : ('a' + i - 10);
+        putchar(hex_char);
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
