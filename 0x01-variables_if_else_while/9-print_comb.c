69ce24f9f0c6a3bacdbb2f22afb432cd319eb67b
Allan-Wachira <aldocky@gmail.com>
2023-10-09 00:35:59 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 0; i < 10; i++) {
-        for (int j = i; j < 10; j++) {
-            // Print the first digit
+    int i, j;
+
+    for (i = 0; i < 10; i++) {
+        for (j = i; j < 10; j++) {
+            /* Print the first digit */
             putchar('0' + i);
-            
-            // Check if we need to print a separator and space
+
+            /* Check if we need to print a separator and space */
             if (i != j) {
                 putchar(',');
                 putchar(' ');
             }
         }
     }
-    
+
     putchar('\n');
 
     return 0;
