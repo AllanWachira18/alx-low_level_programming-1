9fce65505b2d6a0cd4ffe1d315468ea2c36915e1
Allan-Wachira <aldocky@gmail.com>
2023-10-09 00:20:05 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    // Loop from 0 to 15 (base 16)
-    for (int i = 0; i < 16; i++) {
-        // Convert the current number to a lowercase hexadecimal character
+    int i;
+
+    /* Loop from 0 to 15 (base 16) */
+    for (i = 0; i < 16; i++) {
+        /* Convert the current number to a lowercase hexadecimal character */
         char hex_char = (i < 10) ? ('0' + i) : ('a' + i - 10);
 
-        // Use putchar to print the character
+        /* Use putchar to print the character */
         putchar(hex_char);
     }
 
-    // Use putchar to print a newline character
+    /* Use putchar to print a newline character */
     putchar('\n');
 
     return 0;
