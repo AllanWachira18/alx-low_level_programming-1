164b870be0b39211d4bf51cef74c2808a6476c60
Allan-Wachira <aldocky@gmail.com>
2023-10-09 01:05:43 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,52 +1,29 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: Prints all possible different combinations of four digits.
- * Numbers must be separated by ', ' followed by a space.
- * The four digits must be different.
- * 0123, 0132, 0213, 0231, 0312, 0321, 1023, 1032, 1203, 1230, 1302, 1320,
- * 2013, 2031, 2103, 2130, 2301, 2310, 3012, 3021, 3102, 3120, 3201, 3210
- * are considered the same combination of the four digits 0, 1, 2, and 3.
- * Print only the smallest combination of four digits.
- * Numbers should be printed in ascending order, with four digits.
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden).
- * You can only use putchar seven times maximum in your code.
- * You are not allowed to use any variable of type char.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	int a, b, c, d;
+    int first_digit, second_digit, third_digit;
 
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = a + 1; b <= 9; b++)
-		{
-			for (c = b + 1; c <= 9; c++)
-			{
-				for (d = c + 1; d <= 9; d++)
-				{
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(',');
-					putchar(' ');
-					putchar(c + '0');
-					putchar(d + '0');
+    for (first_digit = 0; first_digit <= 7; first_digit++)
+    {
+        for (second_digit = first_digit + 1; second_digit <= 8; second_digit++)
+        {
+            for (third_digit = second_digit + 1; third_digit <= 9; third_digit++)
+            {
+                putchar(first_digit + '0');
+                putchar(second_digit + '0');
+                putchar(third_digit + '0');
 
-					if (!(a == 9 && b == 6 && c == 8 && d == 7))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
+                if (!(first_digit == 7 && second_digit == 8 && third_digit == 9))
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return 0;
 }
