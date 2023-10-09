98e177ad91db49cec948da0d26280ee01c14da29
Allan-Wachira <aldocky@gmail.com>
2023-10-09 01:19:51 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,11 +10,11 @@ int main(void)
         {
             if (num1 != num2)
             {
-                putchar((num1 / 10) + '0');  // Print the tens digit of num1
-                putchar((num1 % 10) + '0');  // Print the ones digit of num1
+                putchar((num1 / 10) + '0');  /* Print the tens digit of num1 */
+                putchar((num1 % 10) + '0');  /* Print the ones digit of num1 */
                 putchar(' ');
-                putchar((num2 / 10) + '0');  // Print the tens digit of num2
-                putchar((num2 % 10) + '0');  // Print the ones digit of num2
+                putchar((num2 / 10) + '0');  /* Print the tens digit of num2 */
+                putchar((num2 % 10) + '0');  /* Print the ones digit of num2 */
 
                 if (num1 != 98 || num2 != 99)
                 {
